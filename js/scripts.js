c55b16915b489b6bf07c7c7e5d6f1a546f489362
Wayne <wayne.manning@tuta.io>
2022-05-27 00:07:42 +0100

    Add scrolltime and animation for both images

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,37 @@ import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
 
 const scrollTracker = document.querySelector(".scroll-tracker");
 
+const animatedImages = document.querySelectorAll('.image-rotate-in');
+
 const scrollTrackingTimeline = new ScrollTimeline({
     source: document.scrollingElement,
     orientation: "block",
     scrollOffsets: [CSS.percent(0), CSS.percent(100)],
 });
+
+animatedImages.forEach(image => {
+    const animatedImageTimeline = new ScrollTimeline({
+        scrollOffsets: [
+            { target: image, edge: "end", threshold: "0" },
+            { target: image, edge: "start", threshold: "1" },
+        ],
+    });
+
+    image.animate(
+        {
+            transform: [
+                "perspective(1000px) rotateX(45deg)",
+                "perspective(1000px) rotate(0)",
+            ],
+            opacity: ["0.5", "1"],
+        },
+        {
+            duration: 1,
+            timeline: animatedImageTimeline,
+        }
+    );
+});
+
 
 scrollTracker.animate(
     {
@@ -17,4 +43,6 @@ scrollTracker.animate(
         timeline: scrollTrackingTimeline,
     }
 );
+
+
 
